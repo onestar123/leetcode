fd2541217ef79d44c78b5ac4ed879f70cda5f748
Schlake <767760477@qq.com>
2018-07-28 16:21:26 +0800

    1

--- 1.py
+++ 1.py
@@ -1,5 +1,16 @@
-s="asdfafe"
-b = []
-for i in s:
-    b.append(i)
-print (b[0])
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict1 = {}
+        for k, i in enumerate(nums):
+            dict1[i] = k
+            if target - i in dict1 and i is not target - i:
+                return [dict1[target - i], dict1[i]]
+        nums = [4, 3, 5, 15]
+        target = 8
+        s = Solution()
+        print(s.twoSum(nums, target))
